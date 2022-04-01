7417aeb2686b566cc7e505687cd01b4d8fcd4307
nesdust <61310895+nesdust@users.noreply.github.com>
2022-04-01 15:35:41 +0200

    Now using accelerometer sensor and magnometer sensor to determine change in degrees

--- app/src/main/java/com/example/compassapplication/DisplayCompassActivity.java
+++ app/src/main/java/com/example/compassapplication/DisplayCompassActivity.java
@@ -26,10 +26,19 @@ public class DisplayCompassActivity extends AppCompatActivity implements SensorE
 
     TextView degreeTextView;
 
-    //
+    // Using both accelerometer and magnetometer (sensor fusion) for more accurate readings
+    // (Also workaround to the deprecation of Sensor.TYPE_ORIENTATION)
+    private Sensor accelerometer;
+    private Sensor magneticField;
 
+    // Most recent reading of accelermeter sensor
     private float[] prevAccReading = new float[3];
+    // Most recent reading of accelermeter sensor
     private float[] prevMagReading = new float[3];
+
+    // rotation matrix and orientation vector used to calc orientation in degrees
+    private float[] rotationMatrix = new float[9];
+    private float[] orientationVector = new float[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +53,68 @@ public class DisplayCompassActivity extends AppCompatActivity implements SensorE
 
         // initializes the android devices sensor handler/manager
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        // Register listeners for accelerometer and magnetometer sensor
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_GAME);
+                //SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(this, magneticField, SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_GAME);
+                //SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if(event == null){
+            return;
+        }
+
+        // Makes deepcopy of the sensor event values to either prevAccReading eller prevMagReading
+        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+            System.arraycopy(event.values, 0, prevAccReading,0, event.values.length);
+        } else if(event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, prevMagReading,0, event.values.length);
+        }
+
+        updateAnimation();
+    }
+
+    private void updateAnimation(){
+
+        boolean valid_reading = SensorManager.getRotationMatrix(rotationMatrix, null, prevAccReading, prevMagReading);
+
+        if(valid_reading){
+            //gets and updates orientation vector
+            float[] orientation = SensorManager.getOrientation(rotationMatrix, orientationVector);
+
+            //return the angle around the z-axis rotated (Azizmuth)
+            float degree = (float) (Math.toDegrees(orientation[0]) + 360) % 360;
+            degree = Math.round(degree * 100) / 100;
+
+            // setText complains when using string concatenation directly in func call
+            String newDegStr = "Heading: " + degree + " degrees";
+            degreeTextView.setText(newDegStr);
+
+            // create a rotation animation (reverse turn degree degrees)
+            RotateAnimation ra = new RotateAnimation(
+                    currentDegree,
+                    -degree,
+                    Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF,
+                    0.5f);
+
+            // how long the animation will take place
+            ra.setDuration(210);
+
+            // set the animation after the end of the reservation status
+            ra.setFillAfter(true);
+
+            // Start the animation
+            image.startAnimation(ra);
+            currentDegree = -degree;
+        }
     }
 
     @Override
@@ -52,48 +123,20 @@ public class DisplayCompassActivity extends AppCompatActivity implements SensorE
 
         // to stop the listener and save battery
         sensorManager.unregisterListener(this);
-        System.out.println("inside onPause");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        // code for system's orientation sensor registered listeners
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR),
-                SensorManager.SENSOR_DELAY_GAME);
-        //System.out.println("inside onResume");
-        //sensorManager.registerListener(this,  sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
-        //        SensorManager.SENSOR_DELAY_GAME);
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-
-        // get the angle around the z-axis rotated
-        System.out.println("event values: " + Math.toDegrees(event.values[2]));
-        float degree = Math.round(Math.toDegrees(event.values[2]));
-
-        String newDegStr = "Heading: " + Float.toString(degree) + " degrees";
-        degreeTextView.setText(newDegStr);
-
-        // create a rotation animation (reverse turn degree degrees)
-        RotateAnimation ra = new RotateAnimation(
-                currentDegree,
-                -degree,
-                Animation.RELATIVE_TO_SELF, 0.5f,
-                Animation.RELATIVE_TO_SELF,
-                0.5f);
-
-        // how long the animation will take place
-        ra.setDuration(210);
-
-        // set the animation after the end of the reservation status
-        ra.setFillAfter(true);
-
-        // Start the animation
-        image.startAnimation(ra);
-        currentDegree = -degree;
-
+        // activate listeners for the sensors accelerometer and magneticField
+        sensorManager.registerListener(this, accelerometer,  SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_GAME);
+                //SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(this, magneticField, SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_GAME);
+                //SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_GAME);
+        // SENSOR_DELAY_GAME
+        // SENSOR_DELAY_FASTEST
+        // SENSOR_DELAY_NORMAL
+        // SENSOR_DELAY_UI
     }
 
     @Override
